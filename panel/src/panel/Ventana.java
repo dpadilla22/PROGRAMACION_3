65a246b01db480e2da1b67dc529049239967aea7
chenaoh <cdhenao@gmail.com>
2024-02-21 18:21:04 -0600

    unidad 2: ejercicio 13

--- panel/src/panel/Ventana.java
+++ panel/src/panel/Ventana.java
@@ -1,10 +1,12 @@
 package panel;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,7 +20,7 @@ public class Ventana extends JFrame {
 	public Ventana(){
 		setVisible(true);
 		setTitle("mi primer ventanita");
-		setSize (500,500);
+		setSize (750,750);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.componentes();
@@ -86,30 +88,39 @@ public class Ventana extends JFrame {
 		
 		
 		JLabel name =new JLabel("Nombre completo: ", SwingConstants.CENTER);
-		name.setBackground(Color.white);
+		name.setFont(new Font("signPainter",Font.BOLD,15));
 		name.setLocation(10,80);
 		name.setSize(180,30);
-		name.setOpaque(true);
+		
 		registro.add(name);
 		
 		JTextField nameField=new JTextField();
 		nameField.setBounds(10, 130, 250, 30);
 		registro.add(nameField);
 		
+		JLabel casillas =new JLabel("Marque las casillas porfavor");
+		casillas.setFont(new Font("signPainter",Font.BOLD,15));
+		casillas.setBounds(10, 170, 350, 40);
+		registro.add(casillas);
+		
+		
+		
 		JCheckBox eleccion=new JCheckBox("gatos"); 
-		eleccion.setBounds(10, 170, 300, 35);
+		eleccion.setBounds(10, 220, 300, 35);
 		registro.add(eleccion);
 		
 		JCheckBox eleccion2=new JCheckBox("perros"); 
-		eleccion2.setBounds(10, 215, 300, 35);
+		eleccion2.setBounds(10, 260, 300, 35);
+		eleccion2.setOpaque(true);
+		eleccion2.setBackground(Color.gray);
 		registro.add(eleccion2);
 		
 		JRadioButton tipo=new JRadioButton("one piece");
-		tipo.setBounds(10, 255, 300, 35);
+		tipo.setBounds(10, 300, 300, 35);
 		registro.add(tipo);
 		
 		JRadioButton tipo2=new JRadioButton("naruto");
-		tipo2.setBounds(10, 295, 300, 35);
+		tipo2.setBounds(10, 345, 300, 35);
 		registro.add(tipo2);
 		
 		ButtonGroup grupo=new ButtonGroup();
@@ -121,14 +132,33 @@ public class Ventana extends JFrame {
 		grupo2.add(eleccion);
 		grupo2.add(eleccion2);
 		
+		JLabel bioTag=new JLabel("describete");
+		bioTag.setFont(new Font("Weird Comic",Font.BOLD,10));
+		bioTag.setBounds(10, 380, 300, 35);
+		registro.add(bioTag);
+		
+		
 		
 		JTextArea bio=new JTextArea(10,10);
-		bio.setLocation(10,345);
-		bio.setSize(300,100);
+		bio.setBounds(10, 410, 300, 35);
 		registro.add(bio);
 		add(login);
 		this.repaint();
 		
+		JLabel bioTag1 =new JLabel("En donde vives");
+		bioTag1.setFont(new Font("Weird Comic",Font.BOLD,10));
+		bioTag1.setBounds(10, 450, 300, 35);
+		registro.add(bioTag1);
+		
+		String locacion[]={"paraiso de sol","camino real","calandrio","santa fe"};
+		
+		JComboBox colonias=new JComboBox(locacion);
+		colonias.setBounds(10, 490, 300, 35);
+		registro.add(colonias);
+		
+		JButton registrar=new JButton("GUARDAR");
+		registrar.setBounds(10, 540, 300, 35);
+		registro.add(registrar);
 	}
 }
 
